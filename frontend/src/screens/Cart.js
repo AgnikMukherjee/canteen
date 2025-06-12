bf7a897cc76f70c2955384900e00de9a638b6cb5
Agnik mukherjee <agnikmukherjee78@gmail.com>
2025-06-12 15:11:57 +0530

    Update Cart.js

--- frontend/src/screens/Cart.js
+++ frontend/src/screens/Cart.js
@@ -16,7 +16,7 @@ if(data.length === 0){
 const handleCheckout = async () => {
     let userEmail = localStorage.getItem("userEmail");
     try {
-        let response = await fetch("https://vercel.com/agnik-mukherjees-projects/canteen/api/orders", {
+        let response = await fetch("https://canteen-alpha.vercel.app/api/orders", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
