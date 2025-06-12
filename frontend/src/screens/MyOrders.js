5de317853ef91070d537d8690ea9080f468b8292
Agnik mukherjee <agnikmukherjee78@gmail.com>
2025-06-12 15:13:18 +0530

    Update MyOrders.js

--- frontend/src/screens/MyOrders.js
+++ frontend/src/screens/MyOrders.js
@@ -7,7 +7,7 @@ export default function MyOrders() {
     useEffect(() => {
         const fetchOrders = async () => {
             try {
-                const response = await fetch("https://vercel.com/agnik-mukherjees-projects/canteen/api/myorders", {
+                const response = await fetch("https://canteen-alpha.vercel.app/api/myorders", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
